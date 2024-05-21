469be73f2bc9ea2077b540da178da0a0768c5edd
Corey <CDartswebdev@gmail.com>
2024-05-21 14:56:55 -0600

    more horesy work

--- tophers/src/components/Carousel.js
+++ tophers/src/components/Carousel.js
@@ -3,28 +3,29 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './Carousel.css';
 import images from '../images';
-import Slide from './Slide';
 
 const MyComponent = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const nextSlide = () => {
-    setCurrentSlide((currentSlide + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide((currentSlide - 1 + images.length) % images.length);
+  const handleThumbnailClick = (index) => {
+    setCurrentSlide(index);
   };
 
   return (
     <div className="carousel">
-      <button className="carousel-arrow carousel-arrow-left" onClick={prevSlide}>&lt;</button>
-      <Carousel showThumbs={true} selectedItem={currentSlide} onChange={setCurrentSlide}>
+      <Carousel 
+        autoPlay={true} 
+        interval={3000} 
+        showThumbs={true} 
+        selectedItem={currentSlide} 
+        onChange={(index) => setCurrentSlide(index)}
+      >
         {images.map((image, index) => (
-          <Slide image={image} index={index} key={index} />
+          <div key={index} onClick={() => handleThumbnailClick(index)}>
+            <img className="carousel-image" src={image} alt={`Slide ${index + 1}`} />
+          </div>
         ))}
       </Carousel>
-      <button className="carousel-arrow carousel-arrow-right" onClick={nextSlide}>&gt;</button>
     </div>
   );
 };
